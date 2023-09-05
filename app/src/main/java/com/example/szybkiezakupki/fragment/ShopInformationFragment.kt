5bfd4185be052be76c2ed6bef11b08feea67e74b
Michał Szczechowicz <michalszczechowicz@gmail.com>
2023-09-05 21:35:04 +0200

    initial commit

--- app/src/main/java/com/example/szybkiezakupki/fragment/ShopInformationFragment.kt
+++ app/src/main/java/com/example/szybkiezakupki/fragment/ShopInformationFragment.kt
@@ -21,7 +21,7 @@ class ShopInformationFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
+//123
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
